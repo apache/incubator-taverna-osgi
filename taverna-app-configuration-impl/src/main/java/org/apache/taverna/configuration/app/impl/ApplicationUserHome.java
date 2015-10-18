fec32dfa02c5f5a493ccf7e712ade177ffc4d5b9
Stian Soiland-Reyes <stain@apache.org>
2015-10-18 13:29:08 +0100

    uk.org.taverna.* -> org.apache.taverna.*

--- taverna-app-configuration-impl/src/main/java/org/apache/taverna/configuration/app/impl/ApplicationUserHome.java
+++ taverna-app-configuration-impl/src/main/java/org/apache/taverna/configuration/app/impl/ApplicationUserHome.java
@@ -18,7 +18,7 @@
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  ******************************************************************************/
-package uk.org.taverna.configuration.app.impl;
+package org.apache.taverna.configuration.app.impl;
 
 import java.io.File;
 
